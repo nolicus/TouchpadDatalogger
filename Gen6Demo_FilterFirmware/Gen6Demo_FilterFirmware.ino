beebfd2d9db53dd266800de04bb3c9590c96b154
Nolan McCulloch <nolan.mcculloch@cirque.com>
2021-10-13 08:46:49 -0600

    Updated ino file so that it streams ptp and filter values as CSV.

--- Gen6Demo_FilterFirmware/Gen6Demo_FilterFirmware.ino
+++ Gen6Demo_FilterFirmware/Gen6Demo_FilterFirmware.ino
@@ -33,10 +33,9 @@ void setup()
 
     pinMode(13, OUTPUT);
     digitalWrite(13, LOW);
-}
 
-uint32_t startTime = 0;
-uint8_t debounceLEDCount = 0; 
+    printPtpFilterHeader();
+}
 
 /** The main structure of the loop is: 
     Wait for the Data Ready line to assert. When it does, read the data (which clears DR) and analyze the data.
@@ -54,132 +53,50 @@ void loop()
         /* Interpret report from module */
         if (dataLoggerPrint_mode_g)
         {
-            calcPrintDataLoggerPacket(&report, fltrData);
+            printPtpFilterReport(report, fltrData);
         }
-
-        // PRINT SAMPLE RATE //
-        // uint32_t samplePeriod = millis() - startTime;
-        // uint32_t sampsPerSecond = 1000 / samplePeriod;
-
-        // Serial.print("Samples Per Second: ");
-        // Serial.print(sampsPerSecond, DEC);
-        // Serial.print("\r\n");
-
-        // Serial.print("Sample Delay: ");
-        // Serial.print(samplePeriod, DEC);
-        // Serial.print("\r\n");
-
-        // Serial.println();  
-
-        // startTime = millis();
-    }
-
-    /* Handle incoming messages from user on serial */
-    if(Serial.available())
-    {
-        char rxChar = Serial.read();
-
-        // uint8_t cmd[50];
-        // getComand(cmd);
-        // if(commandIsValid(cmd))
-        // {
-        //      uint16_t exeCmd = parseCmd(cmd);
-        //      switch (exeCmd)
-        //      {
-                // case 0:     // start streaming data
-                // case 1:     // end streaming data
-        //      default:
-        //          break;
-        //      }
-        // }
     }
 }
 
 /******** Functions for Printing Data ***********/
 
-void printPtpReport(HID_report_t * report)
+void printPtpFilterReport(HID_report_t &hidReport, FilterReport_t &fltrData)
 {
-  Serial.print(F("Report ID:\t0x"));
-  Serial.println(report->reportID, HEX);
-  Serial.print(F("Time:\t"));
-  Serial.println(report->ptp.timeStamp);
-  Serial.print(F("Contact ID:\t"));
-  Serial.println(report->ptp.contactID);
-  Serial.print(F("Confidence:\t")); //Microsoft says "Set when a contact is too large to be a finger" but that is backwards. It's clear if the object is too big.
-  Serial.println(report->ptp.confidence); 
-  Serial.print(F("Tip:\t"));// Set if the contact is on the surface of the digitizer, once this clears you know you have lift-off
-  Serial.println(report->ptp.tip);
-  Serial.print(F("X:\t"));
-  Serial.println(report->ptp.x);
-  Serial.print(F("Y:\t"));
-  Serial.println(report->ptp.y);
-  Serial.print(F("Buttons:\t"));
-  Serial.println(report->ptp.buttons);
-  Serial.print(F("Contact Count:\t"));
-  Serial.println(report->ptp.contactCount); //Total number of contacts to be reported in a given report
-  Serial.println();
-   
+    Serial.print(hidReport.reportID, HEX);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.timeStamp);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.contactID);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.confidence);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.tip);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.x);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.y);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.buttons);
+    Serial.print(",");
+    Serial.print(hidReport.ptp.contactCount); //Total number of contacts to be reported in a given report
+    Serial.print(",");
+    Serial.print(fltrData.filteredX);
+    Serial.print(",");
+    Serial.print(fltrData.filteredY); //Total number of contacts to be reported in a given report
+    Serial.println();
 }
 
-/** Run's and prints any filtered filtered data based on the given PTP reports*/ 
-void printFilterReport(HID_report_t *report)
+void printPtpFilterHeader()
 {
-    // API_KalmanFilter_processData(report); // Apply Kalmanfilter to Report
-    Serial.print(F("Filtered X:\t"));
-    Serial.println(report->filter.filteredX);
-    Serial.print(F("Filtered Y:\t"));
-    Serial.println(report->filter.filteredY);
-    Serial.println(); 
-}
-
-/** Run's and prints any filtered filtered data based on the given PTP reports*/
-void calcPrintDataLoggerPacket(HID_report_t *report, FilterReport_t fltrData)
-{
-    // API_KalmanFilter_processData(report); // Apply Kalmanfilter to Report
-
-    uint8_t buffer[16];
-
-    // clear packet
-    for(uint8_t val : buffer)
-    {
-        val = 0; 
-    }
-
-    // Simple packet header
-    buffer[0] = 0x6A; 
-    buffer[1] = 0xD5; 
-
-    // Simple packet footer
-    buffer[14] = 0x17; 
-    buffer[15] = 0xE2;
-
-    // Payload
-    buffer[2] = report->ptp.timeStamp & 0x00FF;
-    buffer[3] = (report->ptp.timeStamp & 0xFF00) >> 8;
-
-    buffer[4] = report->ptp.x & 0x00FF;
-    buffer[5] = (report->ptp.x & 0xFF00) >> 8;
-
-    buffer[6] = report->ptp.y & 0x00FF;
-    buffer[7] = (report->ptp.y & 0xFF00) >> 8;
-
-    buffer[8] = report->ptp.confidence;
-
-    buffer[9] = fltrData.filteredX & 0x00FF;
-    buffer[10] = (fltrData.filteredX & 0xFF00) >> 8;
-
-    buffer[11] = fltrData.filteredY & 0x00FF;
-    buffer[12] = (fltrData.filteredY & 0xFF00) >> 8;
-
-    // Checksum
-    uint8_t checksum = 0; 
-    for(uint8_t i = 0; i < 13; i++)
-    {
-        checksum += buffer[i];
-    }
-    
-    buffer[13] = checksum; 
-
-    // Send data
-    Serial.write(buffer, 16);
+    Serial.print("Report ID,");
+    Serial.print("Time,");
+    Serial.print("Contact ID,");
+    Serial.print("Confidence,"); //Microsoft says "Set when a contact is too large to be a finger" but that is backwards. It's clear if the object is too big.
+    Serial.print("Tip,");// Set if the contact is on the surface of the digitizer, once this clears you know you have lift-off
+    Serial.print("X,");
+    Serial.print("Y,");
+    Serial.print("Buttons,");
+    Serial.print("Contact Count,");
+    Serial.print("Filtered X,");
+    Serial.println("Filtered Y");
 }
